a6505e5146010daa073343776fc3fe1efafa40ff
Johannes Binder <j.binder.x@gmail.com>
2018-01-19 09:20:31 +0100

    Pause background music during TTS and whistle playback

--- app/src/main/java/at/juggle/sieben/SoundPool.java
+++ app/src/main/java/at/juggle/sieben/SoundPool.java
@@ -1,13 +1,16 @@
 package at.juggle.sieben;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 
 import de.baumann.sieben.R;
 
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
+
 /**
  * Created by dermotte on 28.09.2016.
- *
  */
 
 public class SoundPool {
@@ -15,6 +18,41 @@ public class SoundPool {
     public static int sndWhistle = R.raw.whistle_blow_cc0;
 
     public static void playWhistle(Context context) {
-        MediaPlayer.create(context, R.raw.whistle_blow_cc0).start();
+        final MediaPlayer player = MediaPlayer.create(context, R.raw.whistle_blow_cc0);
+        final AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        final int maxVolume = am.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION);
+        final AudioManager.OnAudioFocusChangeListener afChangeListener = CreateOnAudioFocusChangeListener(player, maxVolume);
+
+        int result = am.requestAudioFocus(afChangeListener,
+                AudioManager.STREAM_NOTIFICATION,
+                AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            player.start();
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    am.abandonAudioFocus(afChangeListener);
+                }
+            });
+        }
+    }
+
+    private static AudioManager.OnAudioFocusChangeListener CreateOnAudioFocusChangeListener(final MediaPlayer player, final int maxVolume) {
+        return new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int focusChange) {
+                if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                    player.stop();
+                } else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
+                    player.pause();
+                } else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                    player.setVolume(maxVolume / 2, maxVolume / 2);
+                } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                    if (player.getCurrentPosition() < player.getDuration() && player.getCurrentPosition() > 0) {
+                        player.start();
+                    }
+                    player.setVolume(maxVolume, maxVolume);
+                }
+            }
+        };
     }
 }
--- app/src/main/java/de/baumann/sieben/helper/TTSManager.java
+++ app/src/main/java/de/baumann/sieben/helper/TTSManager.java
@@ -2,18 +2,26 @@ package de.baumann.sieben.helper;
 
 
 import android.content.Context;
+import android.media.*;
 import android.os.Build;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 
+import java.util.HashMap;
 import java.util.Locale;
+
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
 
 public class TTSManager {
 
     private TextToSpeech mTts = null;
     private boolean isLoaded = false;
+    private Context context = null;
 
     public void init(Context context) {
+        this.context = context;
         try {
             mTts = new TextToSpeech(context, onInitListener);
         } catch (Exception e) {
@@ -53,14 +61,51 @@ public class TTSManager {
 
     public void initQueue(String text) {
         if (isLoaded) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-            } else {
-                //noinspection deprecation
-                mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            final android.media.AudioManager am = (android.media.AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            final int maxVolume = am.getStreamMaxVolume(android.media.AudioManager.STREAM_NOTIFICATION);
+            final android.media.AudioManager.OnAudioFocusChangeListener afChangeListener = CreateOnAudioFocusChangeListener(maxVolume);
+            mTts.stop();
+            int result = am.requestAudioFocus(afChangeListener,
+                    android.media.AudioManager.STREAM_NOTIFICATION,
+                    android.media.AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+            if (result == android.media.AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                String utteranceId = "tts-message";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+                } else {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, utteranceId);
+                    //noinspection deprecation
+                    mTts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+                }
+                mTts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                    @Override
+                    public void onStart(String s) {
+                    }
+
+                    @Override
+                    public void onDone(String s) {
+                        am.abandonAudioFocus(afChangeListener);
+                    }
+
+                    @Override
+                    public void onError(String s) {
+                    }
+                });
             }
         }
         else
             Log.e("error", "TTS Not Initialized");
+    }
+
+    private AudioManager.OnAudioFocusChangeListener CreateOnAudioFocusChangeListener(final int maxVolume) {
+        return new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int focusChange) {
+                if (focusChange == AudioManager.AUDIOFOCUS_LOSS || focusChange == AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                    mTts.stop();
+                }
+            }
+        };
     }
 }
